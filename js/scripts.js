713d60050476edc6fa201e33d517b38e118336b7
Student <example@example.com>
2020-01-22 11:13:03 -0800

    add toggle and empty. This will allow user to fill out form multiple times without duplicating results in hidden div

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-
-});
+    $("form#transportation_survey").submit(function(event){
+      event.preventDefault();
+      $("#work-responses").empty();
+      $("#work-responses").toggle();
+      $("input:checkbox[name=work-transportation]:checked").each(function(){
+        var workTransportationMode = $(this).val();
+        $('#work-responses').append(workTransportationMode + "<br>");
+        console.log(workTransportationMode);
+    });
+    
+    $("#formHide").toggle();
+      
+    });
+  });
