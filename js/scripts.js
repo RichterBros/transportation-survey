4726a6de46177e502f49c7943c69f7fc34106781
Student <example@example.com>
2020-01-22 11:37:32 -0800

    push checkbox values into new arrays

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,20 @@ $(document).ready(function(){
       event.preventDefault();
       $("#work-responses").empty();
       $("#work-responses").toggle();
+      var workResponses = [];
+      console.log(workResponses);
       $("input:checkbox[name=work-transportation]:checked").each(function(){
         var workTransportationMode = $(this).val();
+        workResponses.push(workTransportationMode);
         $('#work-responses').append(workTransportationMode + "<br>");
       });
       $("#fun-responses").empty();
       $("#fun-responses").toggle();
+      var funResponses = [];
+      console.log(funResponses);
       $("input:checkbox[name=fun-transportation]:checked").each(function(){
         var funTransportationMode = $(this).val();
+        funResponses.push(funTransportationMode);
         $('#fun-responses').append(funTransportationMode + "<br>");
       });
       $('#work-boxes, #fun-boxes').toggle();
